f8ca47da295bca996d85ecf11f3100c182639a3f
Aytchinda <aymardrinel@outlook.fr>
2024-07-19 18:12:21 +0200

    Templates Show: affichage des représentations

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -4,14 +4,23 @@
 
 @section('content')
     <h1>Liste des {{ $resource }}</h1>
+
     <ul>
-        @foreach($shows as $show)
-        <li>
-            <a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a>
-            @if($show->bookable)
-            <span>{{ $show->price }} €</span>
-            @endif
-        </li>
-        @endforeach
-        </ul>
-    @endsection
+    @foreach($shows as $show)
+    <li>
+        <a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a>
+        @if($show->bookable)
+        <span>{{ $show->price }} €</span>
+        @endif
+        
+        @if($show->representations->count()==1)
+        - <span>1 représentation</span>
+        @elseif($show->representations->count()>1)
+        - <span>{{ $show->representations->count() }} représentations</span>
+        @else
+        - <em>aucune représentation</em>
+        @endif
+    </li>
+    @endforeach
+    </ul>
+@endsection
