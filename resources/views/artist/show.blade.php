7ab802f4907968178a50b33542b26d12ca103b5b
Aytchinda <aymardrinel@outlook.fr>
2024-08-26 15:29:29 +0200

    Update templates Artist & Type

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -5,15 +5,23 @@
 @section('content')
     <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
 
-    <div><a href="{{ route('artist.edit' ,$artist->id) }}">Modifier</a></div>
+    <h2>Liste des types</h2>
+    <ul>
+    @foreach($artist->types as $type)
+        <li>{{ $type->type }}</li>
+    @endforeach
+    </ul>
+
+
+    <div><a href="{{ route('artist.edit', $artist->id) }}">Modifier</a></div>
+
 
     <form method="post" action="{{ route('artist.delete', $artist->id) }}">
         @csrf
         @method('DELETE')
         <button>Supprimer</button>
-      </form>
-      
+    </form>
+
 
     <nav><a href="{{ route('artist.index') }}">Retour à l'index</a></nav>
 @endsection
-
