f01dae233b0dd9ee3df45bd567db83cebb9ddd74
Aytchinda <aymardrinel@outlook.fr>
2024-09-02 18:09:31 +0200

    create

--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -1,23 +1,30 @@
-<form action="{{ route('reservation.book') }}" method="POST">
-    @csrf
+@extends('home.homepage')
 
-    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+@section('content')
+    <div class="container">
+        <h1>Créer Réservations</h1>
 
-    <div class="form-group">
-        <label for="representation">Choisir une représentation</label>
-        <select name="representation_id" id="representation" class="form-control" required>
-            @foreach($representations as $representation)
-                <option value="{{ $representation->id }}">
-                    {{ $representation->show->title }} - {{ $representation->when }}
-                </option>
-            @endforeach
-        </select>
-    </div>
+        <form action="{{ route('reservation.book') }}" method="POST">
+            @csrf
 
-    <div class="form-group">
-        <label for="seats">Nombre de places</label>
-        <input type="number" name="seats" id="seats" class="form-control" min="1" required>
-    </div>
+            <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
 
-    <button type="submit" class="btn btn-primary">Réserver</button>
-</form>
+            <div class="form-group">
+                <label for="representation">Choisir une représentation</label>
+                <select name="representation_id" id="representation" class="form-control" required>
+                    @foreach ($representations as $representation)
+                        <option value="{{ $representation->id }}">
+                            {{ $representation->show->title }} - {{ $representation->when }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="seats">Nombre de places</label>
+                <input type="number" name="seats" id="seats" class="form-control" min="1" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Réserver</button>
+        </form>
+    @endsection
