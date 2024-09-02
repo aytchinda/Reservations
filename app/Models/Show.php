d9938a49f4b18f4830c3a330d1251d296ae29093
Aytchinda <aymardrinel@outlook.fr>
2024-09-02 13:48:22 +0200

    package flux rss

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Feed\Feedable;
+use Spatie\Feed\FeedItem;
 
-class Show extends Model
+class Show extends Model implements Feedable
 {
     use HasFactory;
-        /**
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
@@ -23,14 +26,14 @@ class Show extends Model
         'price',
     ];
 
-   /**
+    /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'shows';
 
-   /**
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
@@ -38,22 +41,49 @@ class Show extends Model
     public $timestamps = true;
 
     /**
-     * Get the main location of the show
+     * Get the main location of the show.
      */
     public function location()
     {
         return $this->belongsTo(Location::class);
     }
 
+    /**
+     * Get the representations of the show.
+     */
     public function representations()
     {
         return $this->hasMany(Representation::class);
     }
 
+    /**
+     * Get the artist types associated with the show.
+     */
     public function artistTypes()
     {
         return $this->belongsToMany(ArtistType::class);
     }
 
+    /**
+     * Convert the show instance to a FeedItem.
+     */
+    public function toFeedItem(): FeedItem
+    {
+        return FeedItem::create()
+            ->id($this->id)
+            ->title($this->title)
+            ->summary($this->description)
+            ->updated($this->updated_at)
+            ->link(config('app.url') . '/show/' . $this->id)
+            ->authorName('') // Vous pouvez remplir le nom de l'auteur si nécessaire
+            ->authorEmail(''); // Vous pouvez remplir l'email de l'auteur si nécessaire
+    }
 
+    /**
+     * Get all the feed items.
+     */
+    public static function getFeedItems()
+    {
+        return Show::all();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,71 +12,59 @@ use App\Http\Controllers\TypeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Route vers la page d'accueil
+Route::get('/', [HomeController::class, 'homepage']);
 
-
-
-
-Route::get('/',[HomeController::class,'homepage']);
 // Page Home pour les utilisateurs authentifiés
 Route::get('/home', [HomeController::class, 'home'])->middleware(['auth'])->name('home');
-Route::get('/home', [HomeController::class, 'home'])->middleware(['auth'])->name('home.homepage');
 
-//Route Artist
+// Routes Artist
 Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
-Route::get('/artist/{id}', [ArtistController::class, 'show'])  ->where('id', '[0-9]+')->name('artist.show');
-Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])  ->where('id', '[0-9]+')->name('artist.edit');
-Route::put('/artist/{id}', [ArtistController::class, 'update'])  ->where('id', '[0-9]+')->name('artist.update');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])->where('id', '[0-9]+')->name('artist.show');
+Route::get('/artist/edit/{id}', [ArtistController::class, 'edit'])->where('id', '[0-9]+')->name('artist.edit');
+Route::put('/artist/{id}', [ArtistController::class, 'update'])->where('id', '[0-9]+')->name('artist.update');
 Route::get('/artist/create', [ArtistController::class, 'create'])->name('artist.create');
 Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
-Route::delete('/artist/{id}', [ArtistController::class, 'destroy']) ->where('id', '[0-9]+')->name('artist.delete');
+Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])->where('id', '[0-9]+')->name('artist.delete');
 
-
+// Routes Type
 Route::get('/type', [TypeController::class, 'index'])->name('type.index');
-Route::get('/type/{id}', [TypeController::class, 'show'])
-		->where('id', '[0-9]+')->name('type.show');
+Route::get('/type/{id}', [TypeController::class, 'show'])->where('id', '[0-9]+')->name('type.show');
 
-Route::get('/locality', [LocalityController::class, 'index']) ->name('locality_index');
-Route::get('/locality/{id}', [LocalityController::class, 'show'])
-		->where('id', '[0-9]+')->name('locality_show');
-
-Route::get('/role', [RoleController::class, 'index'])->name('role_index');
-Route::get('/role/{id}', [RoleController::class, 'show'])
-		->where('id', '[0-9]+')->name('role_show');
-
+// Routes Locality
 Route::get('/locality', [LocalityController::class, 'index'])->name('locality_index');
-Route::get('/locality/{id}', [LocalityController::class, 'show'])
-->where('id', '[0-9]+')->name('locality_show');
+Route::get('/locality/{id}', [LocalityController::class, 'show'])->where('id', '[0-9]+')->name('locality_show');
 
-//Route Location
-Route::get('location', [LocationController::class, 'index'])->name('location_index');
-Route::get('location/{id}', [LocationController::class, 'show'])
-->where('id', '[0-9]+')->name('location_show');
+// Routes Role
+Route::get('/role', [RoleController::class, 'index'])->name('role_index');
+Route::get('/role/{id}', [RoleController::class, 'show'])->where('id', '[0-9]+')->name('role_show');
 
-//Routes Show
+// Routes Location
+Route::get('/location', [LocationController::class, 'index'])->name('location_index');
+Route::get('/location/{id}', [LocationController::class, 'show'])->where('id', '[0-9]+')->name('location_show');
+
+// Routes Show
 Route::get('/show', [ShowController::class, 'index'])->name('show.index');
-Route::get('/show/{id}', [ShowController::class, 'show'])
-->where('id', '[0-9]+')->name('show.show');
+Route::get('/show/{id}', [ShowController::class, 'show'])->where('id', '[0-9]+')->name('show.show');
 
-//Routes Representation
-Route::get('/representation', [RepresentationController::class, 'index'])
-->name('representation_index');
-Route::get('/representation/{id}', [RepresentationController::class, 'show'])
-->where('id', '[0-9]+')->name('representation_show');
+// Routes Representation
+Route::get('/representation', [RepresentationController::class, 'index'])->name('representation_index');
+Route::get('/representation/{id}', [RepresentationController::class, 'show'])->where('id', '[0-9]+')->name('representation_show');
 
-
+// Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::feeds();
 
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
+// Routes Profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Inclure les routes d'authentification
 require __DIR__.'/auth.php';
